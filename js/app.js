88a3453b3e68192b0365394a0d8e52e8c2fc07df
Andres Vargas <andreselias.vargas@gmail.com>
2021-10-20 18:18:12 -0600

    Weather controller

--- js/app.js
+++ js/app.js
@@ -1,2 +1,33 @@
 'use strict';
 
+const cityForm = document.querySelector('form');
+
+const updateCity = async(city) => {
+  // console.log(city);
+
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  // object shorthand notation
+  return { cityDets, weather };
+
+  // return {
+  //   cityDets: cityDets,
+  //   weather: weather
+  // };
+
+};
+
+cityForm.addEventListener('submit', e => {
+  // prevent default
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //update UI with new city
+  updateCity(city)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
